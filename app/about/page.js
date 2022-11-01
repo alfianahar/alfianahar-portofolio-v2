fd011314148058a5890140a113630cbc4d73c9f3
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-01 23:07:58 +0700

    test fetch data

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,25 @@
-export default function Page() {
+async function getData() {
+    const data = await fetch("https://dummyjson.com/users?limit=6")
+    return data.json()
+}
+
+export default async function Page() {
+    const { users } = await getData();
+    // console.log({ users })
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", height: "100vh", fontSize: "3rem" }}>
+            <div style={{ display: 'flex', justifyContent: "center", alignItems: "center", fontSize: "2rem" }}>
                 <h1>About Page</h1>
+            </div>
+            <div style={{ display: 'grid', gridTemplateColumns: "repeat(3, minmax(0, 1fr))", gap: "1rem", fontSize: "1rem" }}>
+                {
+                    users.map((people) => (
+                        <div key={people.id} style={{ border: "4px solid black", borderRadius: "0.375rem", padding: "1rem" }}>
+                            <img src={people.image} alt={people.maidenName} style={{ padding: "0.5rem" }} />
+                            <h1>{people.firstName} {people.lastName}</h1>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
--- app/navbar.js
+++ app/navbar.js
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav style={{ padding: "1rem", display: "flex", gap: "2rem", position: "fixed   ", top: "0", }}>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/work">Work</Link>
+        <nav style={{ padding: "2rem 4rem", display: "flex", justifyContent: "space-between", position: "fixed", top: "0", left: "0", right: "0" }}>
+            <div>
+                <Link href="/">LOGO</Link>
+            </div>
+            <div >
+                <Link href="/" style={{ marginRight: "1.5rem" }}>Home</Link>
+                <Link href="/about" style={{ marginRight: "1.5rem" }}>About</Link>
+                <Link href="/work" style={{ marginRight: "1.5rem" }}>Work</Link>
+            </div>
         </nav>
     )
 }
