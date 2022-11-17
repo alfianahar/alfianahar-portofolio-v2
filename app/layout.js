a5f9e318b2392809cda577ccb954d774e2e8e1e9
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-17 21:45:12 +0700

    test next/font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-// import { Fira_Sans_Extra_Condensed } from '@next/font/google';
+import { Fira_Sans_Condensed } from '@next/font/google';
 import Navbar from "./navbar";
 import './globals.css'
 
-// const fira = Fira_Sans_Extra_Condensed({
-//     weight: '700',
-// });
+const fira = Fira_Sans_Condensed({
+    weight: '700',
+    subsets: ["latin"]
+});
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={fira.className}>
             <head>
                 <title>Alfian Nahar</title>
             </head>
