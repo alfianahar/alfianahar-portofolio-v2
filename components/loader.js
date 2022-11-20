f628353f6f20470224869dff1c987c1dcbeed4b9
Alfian Nahar <58506749+alfianahar@users.noreply.github.com>
2022-11-20 23:35:45 +0700

    add frame for load

--- components/loader.js
+++ components/loader.js
@@ -22,10 +22,12 @@ export default function Loader() {
             {
                 display &&
                 <div className={`fixed h-screen w-screen bg-white z-50 flex justify-center items-center duration-700 ease-in-out  ${loader ? '' : 'opacity-0 -z-40'}`}>
-                    <div className="inline-flex justify-center animate-load">
+                    <div className="bg-black fixed h-screen w-[200vw] flex justify-center items-center">
+                        <div className="-rotate-[10deg] h-screen w-[200vw] bg-gray-200"></div>
+                    </div>
+                    <div className="inline-flex justify-center items-center animate-load delay-75">
                         <Image src={mylogo} alt="vercel" className="w-[10%] mr-2" />
                         <h1 className="animate-text text-6xl font-black bg-clip-text text-transparent bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 "> ALFIAN NAHAR</h1>
-
                     </div>
                 </div>
             }
